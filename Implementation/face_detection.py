c3b9bfb857a7d485540abeefe24996cdfdf054e2
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-06-25 20:45:35 +0200

    Starting experiments

--- Implementation/face_detection.py
+++ Implementation/face_detection.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('cascade.xml')
 
 img = cv2.imread('02.jpg')
 cv2.imshow('original', img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(img)
+faces = face_cascade.detectMultiScale(gray)
 for (x,y,w,h) in faces:
     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
